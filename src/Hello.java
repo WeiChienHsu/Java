e4ebd1d3b8f2b389e28f009d2cc40ee41c034b18
WeiChien Hsu <weichien711@gmail.com>
2017-12-09 12:53:28 -0600

    Add variable

--- src/Hello.java
+++ src/Hello.java
@@ -1,5 +1,14 @@
 public class Hello {
     public static void main(String[] args) {
-        System.out.println("Hello World!" );
+        double numPound = 200d;
+        double convertedKg = numPound * 0.45359237;
+        System.out.println(convertedKg);
+
+        char myChar = '\u00A9';
+        System.out.println(myChar);
+
+        boolean myBoolean = true;
+        System.out.println(!myBoolean);
+
     }
 }
