305e9369e4d2aa0ce6325a95d61cef5945491517
WeiChien Hsu <weichien711@gmail.com>
2017-12-09 16:04:13 -0600

    add method

--- src/Hello.java
+++ src/Hello.java
@@ -1,14 +1,25 @@
 public class Hello {
-    public static void main(String[] args) {
-        double numPound = 200d;
-        double convertedKg = numPound * 0.45359237;
-        System.out.println(convertedKg);
 
-        char myChar = '\u00A9';
-        System.out.println(myChar);
+    public static void main(String[] args){
+        int score = 100000;
+        int levelCompleted = 8;
+        int bonus = 200;
+        boolean gameOver = true;
 
-        boolean myBoolean = true;
-        System.out.println(!myBoolean);
+        calculateScore(true, 800, 8, 100);
 
+        System.out.println(calculateScore(false, 500, 10, 200));
+
+        }
+
+    public static int calculateScore(boolean gameOver, int score, int levelCompleted, int bonus) {
+        if(gameOver) {
+            int finalScore = score + (bonus * levelCompleted);
+            System.out.println("your final score is: " + finalScore);
+            return finalScore;
+        } else {
+            System.out.println("You haven't finished the game!");
+            return -1;
+        }
     }
 }
