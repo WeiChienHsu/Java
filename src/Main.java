265f3864bef0fc27689b17b858c2775cbc91f20b
WeiChien Hsu <weichien711@gmail.com>
2017-12-09 20:35:45 -0600

    Add Oject with Class

--- src/Main.java
+++ src/Main.java
@@ -1,34 +1,7 @@
 public class Main {
 
-    public static void main(String[] args) {
-        pointOutPrime(21, 7);
-
+    public static void main(String[] args){
+        Car mazda = new Car();
+        Car toyota = new Car();
     }
-
-    public static void pointOutPrime(int num, int start){
-        int count = 1;
-        for ( int n = start; n <= num; n++){
-            if (isPrime(n)){
-                System.out.println( count + ". " + n + " is Prime!");
-                count ++;
-                if(count > 3){
-                    break;
-                }
-            }
-        }
-    }
-
-
-    public static boolean isPrime(int num) {
-        if (num == 1) {
-            return false;
-        }
-        for (int i = 2; i < num; i++) {
-            if (num % i == 0){
-                return false;
-            }
-        }
-        return true;
-    }
-
 }
