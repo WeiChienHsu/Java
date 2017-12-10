728c0b0637156e65bee7cc9434af63013bccaefa
WeiChien Hsu <weichien711@gmail.com>
2017-12-09 20:57:45 -0600

    there's a problem

--- src/Car.java
+++ src/Car.java
@@ -4,6 +4,14 @@ public class Car {
     private String model;
     private String engine;
     private String color;
+
+    public void setModel(String model){
+        this.model = model;
+    }
+
+    public String getModel(){
+        return this.model;
+    }
 }
 
 
--- src/Main.java
+++ src/Main.java
@@ -3,5 +3,7 @@ public class Main {
     public static void main(String[] args){
         Car mazda = new Car();
         Car toyota = new Car();
+        mazda.setModel("MAZDA2");
+        System.out.println("Model is "+ mazda.getModel());
     }
 }
