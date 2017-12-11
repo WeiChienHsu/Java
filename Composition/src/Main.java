98b4ab86fd719bf3d7420b9ddcfc45df38375e0a
WeiChien Hsu <weichien711@gmail.com>
2017-12-10 23:07:50 -0600

    Composition

--- Composition/src/Main.java
+++ Composition/src/Main.java
@@ -1,2 +1,11 @@
 public class Main {
+    public static void main(String[] args){
+        Dimensions dimensions = new Dimensions(20, 20, 5);
+        Case theCase = new Case("220B", "Dell", "240", dimensions);
+        Monitor theMonitor = new Monitor("27inch Beast", "Acer", 27, new Resolution(2539, 1440));
+        Motherboard motherboard = new Motherboard("Bj-200", "Asus", 4, 5, "2v.44");
+
+        PC thePC = new PC(theCase, theMonitor, motherboard);
+        thePC.getMonitor().drawPixelAt(155, 200, "Red");
+    }
 }
