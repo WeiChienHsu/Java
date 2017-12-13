9bda1d0cba3355380dd5f67f6fb83f54113df6cb
WeiChien Hsu <weichien711@gmail.com>
2017-12-13 13:19:32 -0600

    add Scope

---
+++ Packages/src/Main.java
@@ -0,0 +1,9 @@
+import javax.xml.soap.Node;
+
+public class Main {
+    public static void main(String[] args) {
+        Node node = null;
+        org.w3c.dom.Node anotherNode = null;
+
+    }
+}
---
+++ ScopeCheck/src/Main.java
@@ -0,0 +1,10 @@
+public class Main {
+    public static void main(String[] args) {
+        String privateVar = "this is private to Main()";
+
+        ScopeCheck scopeInstance = new ScopeCheck();
+        System.out.println("scopeInstance privateVar is " + scopeInstance.getPrivateVar());
+        System.out.println(privateVar);
+    }
+
+}
